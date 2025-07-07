e3141834f5d98c794f2922ab3afd54cdcb87335e
Vajid Khan <vajidali@buddyboss.com>
2025-07-07 10:51:10 +0400

    custom code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import EmojiPicker from 'rn-emoji-keyboard'
+import { Vimeo } from 'react-native-vimeo-iframe'
+import KeepAwake from 'react-native-keep-awake';
 import { initialize } from "@blocklienterprise/blocklidev";
 
 export const applyCustomCode = (externalCodeSetup) => {
