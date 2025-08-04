39d8dc38aa206a5521e49322699e4ede2054d637
Vajid Khan <vajidali@buddyboss.com>
2025-08-04 12:52:00 +0400

    rn upgrade fix

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import EmojiPicker from 'rn-emoji-keyboard'
-import { Vimeo } from 'react-native-vimeo-iframe'
-import KeepAwake from 'react-native-keep-awake';
-import { initialize } from "@blocklienterprise/blocklidev";
+
 
 export const applyCustomCode = (externalCodeSetup) => {
-	alert(typeof initialize)
+	alert("custom code applied")
 };
