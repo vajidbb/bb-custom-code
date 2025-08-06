8ad9adc9691af52dd0f0381846b035f8c6b9ef00
Vajid Khan <vajidali@buddyboss.com>
2025-08-06 16:20:51 +0400

    custom login

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-
+import React from "react";
+import {Text, TouchableOpacity} from "react-native";
 
 export const applyCustomCode = (externalCodeSetup) => {
-	alert("custom code applied")
+
+	externalCodeSetup.authApi.setLoginButton((props) => (
+		<TouchableOpacity
+			onPress={() => {
+				props.doLogin();
+			}}
+			style={{
+				backgroundColor: "blue",
+				padding: 10,
+				borderRadius: 5,
+			}}
+		>
+			<Text style={{color: "red", fontSize: 20}}>Custom Login Button</Text>
+		</TouchableOpacity>
+	));
+
+	alert("applyCustomCode applied");
+
 };
